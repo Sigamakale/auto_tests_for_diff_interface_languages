5c47c9328ce384d0bda3cdfb454c462820fb50e0
Sigamakale <sigamakale@gmail.com>
2025-10-07 23:23:06 +0400

    Changes in test_items.py

--- test_items.py
+++ test_items.py
@@ -8,4 +8,4 @@ def test_check_basket_button_exists(browser):
     browser.get(link)
     #time.sleep(10)
     btn = browser.find_elements(By.CSS_SELECTOR, 'button.btn-add-to-basket')
-    assert len(btn) != 0, f'Страница не содержит кнопку добавления в корзину'
+    assert btn, 'Страница не содержит кнопку добавления в корзину'
