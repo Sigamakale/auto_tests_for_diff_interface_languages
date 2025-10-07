f1ca4b737753da09a682145a3465db24127cf7e6
Sigamakale <sigamakale@gmail.com>
2025-10-07 21:45:28 +0400

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -6,6 +6,6 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_page(browser):
     browser.get(link)
-    time.sleep(10)
+    #time.sleep(10)
     btn = browser.find_elements(By.CSS_SELECTOR, 'button.btn-add-to-basket')
     assert len(btn) != 0, f'Страница не содержит кнопку добавления в корзину'
