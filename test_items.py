ae07cb33806ca9286d39afb32fc239d35053504b
Sigamakale <sigamakale@gmail.com>
2025-10-07 22:40:15 +0400

    Test name is changed test_items.py

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ import time
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
-def test_page(browser):
+def test_check_basket_button_exists(browser):
     browser.get(link)
     #time.sleep(10)
     btn = browser.find_elements(By.CSS_SELECTOR, 'button.btn-add-to-basket')
